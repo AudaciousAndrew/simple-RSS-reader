bbb3c68d606874e5399d04a23d85e69f5bf78e1a
AudaciousAndrew <pukamirka@gmail.com>
2019-12-22 20:57:35 +0300

    styles

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sqlite3
 import feedparser
+from flask import Flask, render_template, request, redirect, url_for
 
+app = Flask(__name__)
 
 conn = sqlite3.connect('lab6.db');
 
@@ -15,3 +17,15 @@ entry = NewsFeed.entries[1];
 
 print (entry.keys());
 print (f"Post Title :{entry.title}");
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/feed")
+def feed():
+    return render_template("feed.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
